cc95beda60c5d61403eec6e18beac1091a8350b6
Daham <dahampositha33@gmail.com>
2024-03-11 20:40:16 +0530

    Change file modes

--- src/main/java/com/example/checkstylepocproject/CheckstylePocProjectApplication.java
+++ src/main/java/com/example/checkstylepocproject/CheckstylePocProjectApplication.java
@@ -8,8 +8,7 @@ public class CheckstylePocProjectApplication
 
 {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		SpringApplication.run(CheckstylePocProjectApplication.class, args);
 	}
 
