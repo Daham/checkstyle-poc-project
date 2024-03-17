750b6c9d060a8260fdaa26138ff03864d42b4413
Daham <dahampositha33@gmail.com>
2024-03-17 19:58:23 +0530

    Add AnotherConfig file

---
+++ src/main/java/com/example/checkstylepocproject/config/AnotherConfig.java
@@ -0,0 +1,31 @@
+
+/*
+ * Copyright (c) 2024. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.example.checkstylepocproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * The type Another config.
+ */
+@Configuration
+public class AnotherConfig {
+
+  /**
+   * Get obj object.
+   *
+   * @return the object
+   */
+  @Bean
+  public Object getObj() {
+    return new Object();
+  }
+}
