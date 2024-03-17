af67b11f9d18e915ec5c67da9af2d89cfa8ee23f
Daham <dahampositha33@gmail.com>
2024-03-17 19:06:57 +0530

    Add dependancies to build.gradle

--- src/main/java/com/example/checkstylepocproject/CheckstylePocProjectApplication.java
+++ src/main/java/com/example/checkstylepocproject/CheckstylePocProjectApplication.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CheckstylePocProjectApplication
+public class CheckstylePocProjectApplication {
 
-{
-
-	public static void main(String[] args)
-	{
-		SpringApplication.run(CheckstylePocProjectApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CheckstylePocProjectApplication.class, args);
+  }
 
 }
